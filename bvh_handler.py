f52e6761b684d599262ac531622913116c437718
김현수 <2019023436@hanyang.ac.kr>
2022-06-06 15:09:56 +0900

    모든 전역변수 제거
    Former-commit-id: 63ac134dd9fe300d7a3caaeaa17a4514a07abbb3

--- bvh_handler.py
+++ bvh_handler.py
@@ -1,27 +1,28 @@
 import numpy as np
-from Joint import Joint
-from Feature import Feature
-import utils
-
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from scipy.spatial.transform import Rotation as R
 
-joint_list = []
-frame_list = []
-query_vector = Feature()
+from Joint import Joint
+from Feature import Feature
+import utils
+import MyWindow
 
-# The global position of the character on the window
-real_global_position = np.array([0., 0., 0.])
 
-bvh_past_position = np.array([])
+class state:
+    joint_list = []
+    frame_list = []
+    query_vector = Feature()
+    bvh_past_position = np.array([])
+    
+    # The global position of the character on the window
+    real_global_position = np.array([0., 0., 0.])
 
 
 def parsing_bvh(bvh):
-    global frame_list, joint_list #, num_of_frames
-    frameTime = 0
-    frame_list = []
 
+    frameTime = 0
+    state.frame_list = []
     num_of_frames = 0
 
     line = bvh.readline().split()
@@ -29,7 +30,7 @@ def parsing_bvh(bvh):
     if line[0] == 'HIERARCHY':
         line = bvh.readline().split()
         if line[0] == 'ROOT':
-            joint_list = []
+            state.joint_list = []
             buildJoint(bvh, line[1])  # build ROOT and other joints
 
     line = bvh.readline().split()
@@ -45,17 +46,15 @@ def parsing_bvh(bvh):
         for i in range(num_of_frames):
             line = bvh.readline().split()
             line = list(map(float, line))
-            frame_list.append(line)
-        last = [0] * len(frame_list[0])
-        frame_list.append(last)
+            state.frame_list.append(line)
+        last = [0] * len(state.frame_list[0])
+        state.frame_list.append(last)
 
     FPS = int(1 / frameTime)
     return FPS
 
 
 def buildJoint(bvh, joint_name):
-    global joint_list
-    # joint_list = []
 
     line = bvh.readline().split()  # remove '{'
     newJoint = Joint(joint_name)
@@ -64,16 +63,16 @@ def buildJoint(bvh, joint_name):
     if "Foot" in joint_name:
         newJoint.set_is_foot(True)
 
-    newJoint.set_index(len(joint_list))
+    newJoint.set_index(len(state.joint_list))
 
-    joint_list.append(newJoint)
+    state.joint_list.append(newJoint)
 
     line = bvh.readline().split()
     if line[0] == 'OFFSET':
         offset = np.array(list(map(float, line[1:])), dtype='float32')
         if joint_name != "Hips" and np.sqrt(np.dot(offset, offset)) > Joint.resize:
             Joint.resize = np.sqrt(np.dot(offset, offset))
-            print("####1", Joint.resize)
+
         newJoint.set_offset(offset)
 
     line = bvh.readline().split()
@@ -90,12 +89,10 @@ def buildJoint(bvh, joint_name):
             line = bvh.readline().split()
             if line[0] == 'OFFSET':
                 offset = np.array(list(map(float, line[1:])), dtype='float32')
-                print(offset)
-                print("####????", Joint.resize)
-                print("####????2", np.sqrt(np.dot(offset, offset)))
+
                 if np.sqrt(np.dot(offset, offset)) > Joint.resize:
                     Joint.resize = np.sqrt(np.dot(offset, offset))
-                    print("####2", Joint.resize)
+
                 newJoint.set_end_site(offset)
             line = bvh.readline().split()  # remove '}'
 
@@ -108,9 +105,6 @@ def buildJoint(bvh, joint_name):
 # the parentMatrix enters identity(4) when drawing a root joint
 
 def drawJoint(parentMatrix, joint, rootMatrix=None):
-    from MyWindow import curFrame
-    from MyWindow import timeStep
-    global real_global_position, bvh_past_position
 
     glPushMatrix()
     newMatrix = np.identity(4)
@@ -129,14 +123,14 @@ def drawJoint(parentMatrix, joint, rootMatrix=None):
     # ROOT
     if len(joint.get_channel()) == 6:
 
-        if len(bvh_past_position) != 0: # Continuous motion playback received via the QnA function
-            real_global_position += np.array(curFrame[:3]) - bvh_past_position
+        if len(state.bvh_past_position) != 0: # Continuous motion playback received via the QnA function
+            state.real_global_position += np.array(MyWindow.state.curFrame[:3]) - state.bvh_past_position
         else:   # if QnA is newly called
-            real_global_position[1] = curFrame[1]   
-        bvh_past_position = curFrame[:3]
+            state.real_global_position[1] = MyWindow.state.curFrame[1]   
+        state.bvh_past_position = MyWindow.state.curFrame[:3]
 
-        # ROOTPOSITION = np.array(curFrame[:3], dtype='float32')
-        ROOTPOSITION = np.array(real_global_position, dtype='float32')
+        # ROOTPOSITION = np.array(MyWindow.state.curFrame[:3], dtype='float32')
+        ROOTPOSITION = np.array(state.real_global_position, dtype='float32')
         ROOTPOSITION /= Joint.resize
 
         # move root's transformation matrix's origin using translation data
@@ -146,7 +140,7 @@ def drawJoint(parentMatrix, joint, rootMatrix=None):
 
         for i in range(3, 6):
             if joint.get_channel()[i].upper() == 'XROTATION':
-                xr = curFrame[i]
+                xr = MyWindow.state.curFrame[i]
                 xr = np.radians(xr)
                 Rx = np.array([[1., 0., 0., 0.],
                                [0, np.cos(xr), -np.sin(xr), 0],
@@ -154,7 +148,7 @@ def drawJoint(parentMatrix, joint, rootMatrix=None):
                                [0., 0., 0., 1.]])
                 newMatrix = newMatrix @ Rx
             elif joint.get_channel()[i].upper() == 'YROTATION':
-                yr = curFrame[i]
+                yr = MyWindow.state.curFrame[i]
                 yr = np.radians(yr)
                 Ry = np.array([[np.cos(yr), 0, np.sin(yr), 0.],
                                [0, 1, 0, 0],
@@ -162,7 +156,7 @@ def drawJoint(parentMatrix, joint, rootMatrix=None):
                                [0., 0., 0., 1.]])
                 newMatrix = newMatrix @ Ry
             elif joint.get_channel()[i].upper() == 'ZROTATION':
-                zr = curFrame[i]
+                zr = MyWindow.state.curFrame[i]
                 zr = np.radians(zr)
                 Rz = np.array([[np.cos(zr), -np.sin(zr), 0, 0],
                                [np.sin(zr), np.cos(zr), 0, 0],
@@ -175,7 +169,7 @@ def drawJoint(parentMatrix, joint, rootMatrix=None):
         index = joint.get_index()
         for i in range(3):
             if joint.get_channel()[i].upper() == 'XROTATION':
-                xr = curFrame[(index + 1) * 3 + i]
+                xr = MyWindow.state.curFrame[(index + 1) * 3 + i]
                 xr = np.radians(xr)
                 Rx = np.array([[1., 0., 0., 0.],
                                [0, np.cos(xr), -np.sin(xr), 0],
@@ -184,7 +178,7 @@ def drawJoint(parentMatrix, joint, rootMatrix=None):
                 newMatrix = newMatrix @ Rx
 
             elif joint.get_channel()[i].upper() == 'YROTATION':
-                yr = curFrame[(index + 1) * 3 + i]
+                yr = MyWindow.state.curFrame[(index + 1) * 3 + i]
                 yr = np.radians(yr)
                 Ry = np.array([[np.cos(yr), 0, np.sin(yr), 0.],
                                [0, 1, 0, 0],
@@ -193,7 +187,7 @@ def drawJoint(parentMatrix, joint, rootMatrix=None):
                 newMatrix = newMatrix @ Ry
 
             elif joint.get_channel()[i].upper() == 'ZROTATION':
-                zr = curFrame[(index + 1) * 3 + i]
+                zr = MyWindow.state.curFrame[(index + 1) * 3 + i]
                 zr = np.radians(zr)
                 Rz = np.array([[np.cos(zr), -np.sin(zr), 0, 0],
                                [np.sin(zr), np.cos(zr), 0, 0],
@@ -223,7 +217,6 @@ def drawJoint(parentMatrix, joint, rootMatrix=None):
         # get root local position and root local velocity
         new_root_local_position = (rootMatrix.T @ global_position)[:3]  # local to root joint
         past_root_local_position = joint.get_root_local_position()  # local to root joint
-        # root_local_velocity = ((new_root_local_position - past_root_local_position) / timeStep)
         root_local_velocity = ((new_root_local_position - past_root_local_position) * 30)   # 30 is FPS of LaFAN1
 
         # get root local rotation and root local angular velocity
@@ -312,18 +305,16 @@ def drawJoint(parentMatrix, joint, rootMatrix=None):
 
 
 def set_query_vector(key_input = None):
-    global joint_list, query_vector
-    from MyWindow import curFrame
 
     two_foot_position = []
     two_foot_velocity = []
     hip_velocity = []
 
-    print("root orientation",curFrame[3:6])
+    print("root orientation", MyWindow.state.curFrame[3:6])
 
     # future facing direction setting
     default_facing_direction = np.array([1., 0., 0.])
-    rotation_current = R.from_euler('zxy', curFrame[3:6], degrees=True)
+    rotation_current = R.from_euler('zxy', MyWindow.state.curFrame[3:6], degrees=True)
     
 
     if key_input is not None:
@@ -357,26 +348,26 @@ def set_query_vector(key_input = None):
     futurePosition = temp * default_future_trajectory
     futurePosition = futurePosition[:, 0::2]
 
-    for joint in joint_list:
+    for joint in state.joint_list:
         if joint.get_is_root():
             hip_velocity.append(joint.get_root_local_velocity())
         elif joint.get_is_foot():
             two_foot_position.append(joint.get_root_local_position())
             two_foot_velocity.append(joint.get_root_local_velocity())
 
-    query_vector.set_future_position(np.array(futurePosition).reshape(6, ))
-    query_vector.set_future_direction(np.array(future2Directions).reshape(6, ))
+    state.query_vector.set_future_position(np.array(futurePosition).reshape(6, ))
+    state.query_vector.set_future_direction(np.array(future2Directions).reshape(6, ))
     
     # test future info setting
-    # query_vector.set_future_position(np.zeros_like(futurePosition).reshape(6, ))
-    # query_vector.set_future_direction(np.zeros_like(future2Directions).reshape(6, ))
+    # state.query_vector.set_future_position(np.zeros_like(futurePosition).reshape(6, ))
+    # state.query_vector.set_future_direction(np.zeros_like(future2Directions).reshape(6, ))
 
-    query_vector.set_foot_position(np.array(two_foot_position).reshape(6, ))
-    query_vector.set_foot_velocity(np.array(two_foot_velocity).reshape(6, ))
-    query_vector.set_hip_velocity(np.array(hip_velocity).reshape(3, ))
+    state.query_vector.set_foot_position(np.array(two_foot_position).reshape(6, ))
+    state.query_vector.set_foot_velocity(np.array(two_foot_velocity).reshape(6, ))
+    state.query_vector.set_hip_velocity(np.array(hip_velocity).reshape(3, ))
 
-    return query_vector.get_feature_list()
+    return state.query_vector.get_feature_list()
 
 def reset_bvh_past_postion():
     global bvh_past_position
-    bvh_past_position = np.array([])
+    state.bvh_past_position = np.array([])
