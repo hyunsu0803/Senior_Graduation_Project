972059a902d93b2431f3dad6582eca770d5d63c1
김현수 <2019023436@hanyang.ac.kr>
2022-06-06 14:08:04 +0900

    dataset_handler 전역변수 제거
    Former-commit-id: 1bad28108b9ce8dd9d8ae0c23cfcf76ea96dd9c8

--- dataset_handler.py
+++ dataset_handler.py
@@ -9,45 +9,46 @@ import os
 from scipy.spatial import cKDTree
 import pickle
 
-joint_list = []
-frame_list = []
-feature_list = []
-timeStep = 0.2
-line_index = 0
-curFrame = []
-futureFrames = [None, None, None]
-futurePosition = [[None], [None], [None]]
-futureDirection = [[None], [None], [None]]
+
+class status:
+    joint_list = []
+    frame_list = []
+    feature_list = []
+    timeStep = 0.2
+    line_index = 0
+    curFrame = []
+    futureFrames = [None, None, None]
+    futurePosition = [[None], [None], [None]]
+    futureDirection = [[None], [None], [None]]
 
 
 def parsing_bvh(bvh):
-    global frame_list, joint_list, line_index #,num_of_frames
     frameTime = 0
-    frame_list = []
-    line_index = 0
+    status.frame_list = []
+    status.line_index = 0
 
     num_of_frames = 0
 
     line = bvh.readline().split()
-    line_index += 1
+    status.line_index += 1
 
     if line[0] == 'HIERARCHY':
         line = bvh.readline().split()
-        line_index += 1
+        status.line_index += 1
         if line[0] == 'ROOT':
-            joint_list = []
+            status.joint_list = []
             buildJoint(bvh, line[1])  # build ROOT and other joints
 
     line = bvh.readline().split()
-    line_index += 1
+    status.line_index += 1
 
     if line[0] == 'MOTION':
         line = bvh.readline().split()
-        line_index += 1
+        status.line_index += 1
         if line[0] == 'Frames:':
             num_of_frames = int(line[1])
         line = bvh.readline().split()
-        line_index += 1
+        status.line_index += 1
 
         if line[0] == 'Frame' and line[1] == 'Time:':
             frameTime = float(line[2])
@@ -55,9 +56,9 @@ def parsing_bvh(bvh):
         for i in range(num_of_frames):
             line = bvh.readline().split()
             line = list(map(float, line))
-            frame_list.append(line)
-        last = [0] * len(frame_list[0])   
-        frame_list.append(last)
+            status.frame_list.append(line)
+        last = [0] * len(status.frame_list[0])   
+        status.frame_list.append(last)
 
     FPS = int(1 / frameTime)
 
@@ -65,23 +66,21 @@ def parsing_bvh(bvh):
 
 
 def buildJoint(bvh, joint_name):
-    global joint_list, line_index
-    # joint_list = []
 
     line = bvh.readline().split()  # remove '{'
-    line_index += 1
+    status.line_index += 1
     newJoint = Joint(joint_name)
 
     # check if it's foot joint
     if "Foot" in joint_name:
         newJoint.set_is_foot(True)
 
-    newJoint.set_index(len(joint_list))
+    newJoint.set_index(len(status.joint_list))
 
-    joint_list.append(newJoint)
+    status.joint_list.append(newJoint)
 
     line = bvh.readline().split()
-    line_index += 1
+    status.line_index += 1
     if line[0] == 'OFFSET':
         offset = np.array(list(map(float, line[1:])), dtype='float32')
         if np.sqrt(np.dot(offset, offset)) > Joint.resize:
@@ -89,35 +88,34 @@ def buildJoint(bvh, joint_name):
         newJoint.set_offset(offset)
 
     line = bvh.readline().split()
-    line_index += 1
+    status.line_index += 1
     if line[0] == 'CHANNELS':
         newJoint.set_channel(line[2:])
 
     while True:
         line = bvh.readline().split()
-        line_index += 1
+        status.line_index += 1
         if line[0] == 'JOINT':
             newJoint.append_child_joint(buildJoint(bvh, line[1]))
 
         elif line[0] == 'End' and line[1] == 'Site':
             line = bvh.readline().split()  # remove '{'
-            line_index += 1
+            status.line_index += 1
             line = bvh.readline().split()
-            line_index += 1
+            status.line_index += 1
             if line[0] == 'OFFSET':
                 offset = np.array(list(map(float, line[1:])), dtype='float32')
                 if np.sqrt(np.dot(offset, offset)) > Joint.resize:
                     Joint.resize = np.sqrt(np.dot(offset, offset))
                 newJoint.set_end_site(offset)
             line = bvh.readline().split()  # remove '}'
-            line_index += 1
+            status.line_index += 1
 
         elif line[0] == '}':
             return newJoint
 
 
 def set_joint_feature(joint, parentMatrix, rootMatrix=None):
-    global curFrame, timeStep, futurePosition, futureDirection
     newMatrix = np.identity(4)
     # cur_position = [0, 0, 0, 1]
 
@@ -131,7 +129,7 @@ def set_joint_feature(joint, parentMatrix, rootMatrix=None):
     # channel rotation
     # ROOT
     if len(joint.get_channel()) == 6:
-        ROOTPOSITION = np.array(curFrame[:3], dtype='float32')
+        ROOTPOSITION = np.array(status.curFrame[:3], dtype='float32')
         ROOTPOSITION /= Joint.resize
 
         # move root's transformation matrix's origin using translation data
@@ -141,7 +139,7 @@ def set_joint_feature(joint, parentMatrix, rootMatrix=None):
 
         for i in range(3, 6):
             if joint.get_channel()[i].upper() == 'XROTATION':
-                xr = curFrame[i]
+                xr = status.curFrame[i]
                 xr = np.radians(xr)
                 Rx = np.array([[1., 0., 0., 0.],
                                [0, np.cos(xr), -np.sin(xr), 0],
@@ -149,7 +147,7 @@ def set_joint_feature(joint, parentMatrix, rootMatrix=None):
                                [0., 0., 0., 1.]])
                 newMatrix = newMatrix @ Rx
             elif joint.get_channel()[i].upper() == 'YROTATION':
-                yr = curFrame[i]
+                yr = status.curFrame[i]
                 yr = np.radians(yr)
                 Ry = np.array([[np.cos(yr), 0, np.sin(yr), 0.],
                                [0, 1, 0, 0],
@@ -157,7 +155,7 @@ def set_joint_feature(joint, parentMatrix, rootMatrix=None):
                                [0., 0., 0., 1.]])
                 newMatrix = newMatrix @ Ry
             elif joint.get_channel()[i].upper() == 'ZROTATION':
-                zr = curFrame[i]
+                zr = status.curFrame[i]
                 zr = np.radians(zr)
                 Rz = np.array([[np.cos(zr), -np.sin(zr), 0, 0],
                                [np.sin(zr), np.cos(zr), 0, 0],
@@ -170,7 +168,7 @@ def set_joint_feature(joint, parentMatrix, rootMatrix=None):
         index = joint.get_index()
         for i in range(3):
             if joint.get_channel()[i].upper() == 'XROTATION':
-                xr = curFrame[(index + 1) * 3 + i]
+                xr = status.curFrame[(index + 1) * 3 + i]
                 xr = np.radians(xr)
                 Rx = np.array([[1., 0., 0., 0.],
                                [0, np.cos(xr), -np.sin(xr), 0],
@@ -179,7 +177,7 @@ def set_joint_feature(joint, parentMatrix, rootMatrix=None):
                 newMatrix = newMatrix @ Rx
 
             elif joint.get_channel()[i].upper() == 'YROTATION':
-                yr = curFrame[(index + 1) * 3 + i]
+                yr = status.curFrame[(index + 1) * 3 + i]
                 yr = np.radians(yr)
                 Ry = np.array([[np.cos(yr), 0, np.sin(yr), 0.],
                                [0, 1, 0, 0],
@@ -188,7 +186,7 @@ def set_joint_feature(joint, parentMatrix, rootMatrix=None):
                 newMatrix = newMatrix @ Ry
 
             elif joint.get_channel()[i].upper() == 'ZROTATION':
-                zr = curFrame[(index + 1) * 3 + i]
+                zr = status.curFrame[(index + 1) * 3 + i]
                 zr = np.radians(zr)
                 Rz = np.array([[np.cos(zr), -np.sin(zr), 0, 0],
                                [np.sin(zr), np.cos(zr), 0, 0],
@@ -203,23 +201,23 @@ def set_joint_feature(joint, parentMatrix, rootMatrix=None):
     # set parent's global position (if it is root joint, parent_position is current_position)
     if joint.get_is_root():
         parent_position = global_position
-        futurePosition = [None, None, None]
-        futureDirection = [None, None, None]
+        status.futurePosition = [None, None, None]
+        status.futureDirection = [None, None, None]
 
         for i in range(0, 3):
             temp = np.array([0, 0, 0, 1])
-            temp[:3] = futureFrames[i][:3]
-            futurePosition[i] = np.linalg.inv(transform_matrix) @ temp
+            temp[:3] = status.futureFrames[i][:3]
+            status.futurePosition[i] = np.linalg.inv(transform_matrix) @ temp
 
-            futurePosition[i] = futurePosition[i][0::2]
+            status.futurePosition[i] = status.futurePosition[i][0::2]
             
             default_facing_direction = np.array([1., 0., 0.])
-            rotation_current = R.from_euler('zxy', curFrame[3:6], degrees=True)
-            rotation_future = R.from_euler('zxy', futureFrames[i][3:6], degrees=True)
+            rotation_current = R.from_euler('zxy', status.curFrame[3:6], degrees=True)
+            rotation_future = R.from_euler('zxy', status.futureFrames[i][3:6], degrees=True)
             rotation_current = np.array(rotation_current.as_matrix())
             rotation_future = np.array(rotation_future.as_matrix())
-            futureDirection[i] = R.from_matrix(rotation_current.T @ rotation_future).as_matrix() @ default_facing_direction
-            futureDirection[i] = futureDirection[i][0::2]
+            status.futureDirection[i] = R.from_matrix(rotation_current.T @ rotation_future).as_matrix() @ default_facing_direction
+            status.futureDirection[i] = status.futureDirection[i][0::2]
 
     else:
         parent_position = parentMatrix @ np.array([0., 0., 0., 1.])
@@ -236,7 +234,7 @@ def set_joint_feature(joint, parentMatrix, rootMatrix=None):
         # get root local position and root local velocity
         new_root_local_position = (rootMatrix.T @ global_position)[:3]  # local to root joint
         past_root_local_position = joint.get_root_local_position()  # local to root joint
-        root_local_velocity = ((new_root_local_position - past_root_local_position) / timeStep)
+        root_local_velocity = ((new_root_local_position - past_root_local_position) / status.timeStep)
 
         # get root local rotation and root local angular velocity
         new_root_local_rotation_matrix = (rootMatrix.T @ transform_matrix)[:3, :3]
@@ -257,21 +255,19 @@ def set_joint_feature(joint, parentMatrix, rootMatrix=None):
 
 
 def set_feature_vector(feature_vector):
-    global joint_list
-
     two_foot_position = []
     two_foot_velocity = []
     hip_velocity = []
 
-    for joint in joint_list:
+    for joint in status.joint_list:
         if joint.get_is_root():
             hip_velocity.append(joint.get_root_local_velocity())
         elif joint.get_is_foot():
             two_foot_position.append(joint.get_root_local_position())
             two_foot_velocity.append(joint.get_root_local_velocity())
 
-    feature_vector.set_future_position(np.array(futurePosition).reshape(6, ))
-    feature_vector.set_future_direction(np.array(futureDirection).reshape(6, ))
+    feature_vector.set_future_position(np.array(status.futurePosition).reshape(6, ))
+    feature_vector.set_future_direction(np.array(status.futureDirection).reshape(6, ))
 
     # test future info setting
     # feature_vector.set_future_position(np.zeros_like(futurePosition).reshape(6, ))
@@ -282,8 +278,7 @@ def set_feature_vector(feature_vector):
 
 
 def main():
-    global curFrame, feature_list, futureFrames #joint_list
-    curFrame = []
+    status.curFrame = []
     Joint.resize = 1
 
     bvh_dir = './lafan2/'
@@ -300,21 +295,21 @@ def main():
 
             # db_index_info.txt 
             num_of_frames, FPS = parsing_bvh(bvh)
-            info = ' '.join([str(db_index), bvh_name, str(line_index), str(FPS)+'\n'])
+            info = ' '.join([str(db_index), bvh_name, str(status.line_index), str(FPS)+'\n'])
             db_index_info.write(info)
 
             db_index += num_of_frames - FPS 
 
-            for i in range(0, len(frame_list) - FPS -1):
-                curFrame = frame_list[i]
-                futureFrames = []  
+            for i in range(0, len(status.frame_list) - FPS -1):
+                status.curFrame = status.frame_list[i]
+                status.futureFrames = []  
                 for j in [int(FPS/3), int(FPS/3*2), int(FPS)]:    
-                    if i + j < len(frame_list):
-                        futureFrames.append(frame_list[i + j])
-                set_joint_feature(joint_list[0], np.identity(4), None)
+                    if i + j < len(status.frame_list):
+                        status.futureFrames.append(status.frame_list[i + j])
+                set_joint_feature(status.joint_list[0], np.identity(4), None)
                 feature_vector = Feature()
                 set_feature_vector(feature_vector)
-                feature_list.append(feature_vector)
+                status.feature_list.append(feature_vector)
 
                 data.append(feature_vector.get_feature_list())
 
