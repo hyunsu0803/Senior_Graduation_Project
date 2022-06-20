483a7924ac7407af3a01e15757cd0ede1916b22a
jbok6825 <jbok6825@naver.com>
2022-06-20 16:30:44 +0900

    check walk_subject1.bvh query vector

--- drawTpose.py
+++ drawTpose.py
@@ -1,7 +1,7 @@
 from bvh_handler import parsing_bvh
 def drawTpose():
     paths = []
-    paths.append('./lafan1/aiming1_subject1.bvh')
+    paths.append('./lafan2/walk1_subject1.bvh')
 
     with open(paths[0], 'r') as file:
         FPS = parsing_bvh(file)
